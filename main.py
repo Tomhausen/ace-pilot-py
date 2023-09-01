c99d9b935d5fe97fab798bff28112e2766519a24
Tom Moran <62551154+Tomhausen@users.noreply.github.com>
2023-09-01 11:03:48 +0100

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,106 @@
+@namespace
+class SpriteKind:
+    player_projectile = SpriteKind.create()
+    enemy_projectile = SpriteKind.create()
+
+# variables
+player_speed = 50
+player_turn = 3
+projectile_speed = 150
+enemy_speed = 50
+enemy_turn = 3
+
+# sprites
+player_plane = sprites.create(assets.image("player"), SpriteKind.player)
+transformSprites.rotate_sprite(player_plane, 90)
+
+# setup
+scene.set_background_color(9)
+scene.set_tile_map_level(assets.tilemap("level"))
+scene.camera_follow_sprite(player_plane)
+
+def fire(sprite: Sprite, speed: number, kind):
+    proj = sprites.create(assets.image("projectile"), kind)
+    proj.set_flag(SpriteFlag.DESTROY_ON_WALL, True)
+    proj.set_position(sprite.x, sprite.y)
+    calculate_velocity(sprite, proj, speed)
+
+def player_fire():
+    fire(player_plane, projectile_speed, SpriteKind.player_projectile)
+controller.A.on_event(ControllerButtonEvent.PRESSED, player_fire)
+
+def place_sprite(sprite: Sprite):
+    col = randint(1, grid.num_columns())
+    row = randint(1, grid.num_rows())
+    tiles.place_on_tile(sprite, tiles.get_tile_location(col, row))
+    if spriteutils.distance_between(sprite, player_plane) < 120:
+        place_sprite(sprite)
+
+def get_dir_to_player(enemy: Sprite):
+    target_dir = spriteutils.angle_from(enemy, player_plane)
+    target_dir = spriteutils.radians_to_degrees(target_dir) + 90
+    return target_dir
+
+def spawn_enemy():
+    enemy = sprites.create(assets.image("enemy"), SpriteKind.enemy)
+    sprites.set_data_number(enemy, "turn", 0)
+    transformSprites.rotate_sprite(enemy, 90)
+    place_sprite(enemy)
+    transformSprites.rotate_sprite(enemy, get_dir_to_player(enemy))
+game.on_update_interval(4000, spawn_enemy)
+
+def destroy_enemy(proj, enemy):
+    info.change_score_by(100)
+    enemy.destroy()
+sprites.on_overlap(SpriteKind.player_projectile, SpriteKind.enemy, destroy_enemy)
+
+def take_damage(proj, player):
+    proj.destroy()
+    info.change_life_by(-1)
+sprites.on_overlap(SpriteKind.enemy_projectile, SpriteKind.player, take_damage)
+
+def hit_edge(sprite, location):
+    transformSprites.change_rotation(sprite, 180)
+scene.on_hit_wall(SpriteKind.player, hit_edge)
+scene.on_hit_wall(SpriteKind.enemy, hit_edge)
+
+def calculate_velocity(direction_sprite: Sprite, sprite: Sprite, speed: number):
+    direction = transformSprites.get_rotation(direction_sprite)
+    direction = spriteutils.degrees_to_radians(direction)
+    sprite.vx = Math.sin(direction) * speed
+    sprite.vy = Math.cos(direction) * -speed
+
+def player_controls():
+    if controller.up.is_pressed():
+        transformSprites.change_rotation(player_plane, -player_turn)
+    elif controller.down.is_pressed():
+        transformSprites.change_rotation(player_plane, player_turn)
+    calculate_velocity(player_plane, player_plane, player_speed)
+
+def far_from_player(enemy: Sprite):
+    direction = transformSprites.get_rotation(enemy)
+    if (get_dir_to_player(enemy) - direction) < 3:
+        sprites.set_data_number(enemy, "turn", -enemy_turn)
+    if (get_dir_to_player(enemy) - direction) > 3:
+        sprites.set_data_number(enemy, "turn", enemy_turn)
+
+def enemy_behaviour(enemy: Sprite):
+    if spriteutils.distance_between(enemy, player_plane) > 80:
+        far_from_player(enemy)
+    elif spriteutils.distance_between(enemy, player_plane) < 40:
+        sprites.set_data_number(enemy, "turn", enemy_turn)
+    else:
+        sprites.set_data_number(enemy, "turn", 0)
+    transformSprites.change_rotation(enemy, sprites.read_data_number(enemy, "turn"))
+    calculate_velocity(enemy, enemy, enemy_speed)
+    if randint(1, 150) == 1:
+        fire(enemy, projectile_speed - 50, SpriteKind.enemy_projectile)
+
+def tick():
+    player_controls()
+    for enemy in sprites.all_of_kind(SpriteKind.enemy):
+        enemy_behaviour(enemy)
+game.on_update(tick)
+
+# gh: could do the smoke thing?
+# round based with messages
---
+++ main.ts
@@ -0,0 +1,117 @@
+namespace SpriteKind {
+    export const player_projectile = SpriteKind.create()
+    export const enemy_projectile = SpriteKind.create()
+}
+
+//  variables
+let player_speed = 50
+let player_turn = 3
+let projectile_speed = 150
+let enemy_speed = 50
+let enemy_turn = 3
+//  sprites
+let player_plane = sprites.create(assets.image`player`, SpriteKind.Player)
+transformSprites.rotateSprite(player_plane, 90)
+//  setup
+scene.setBackgroundColor(9)
+scene.setTileMapLevel(assets.tilemap`level`)
+scene.cameraFollowSprite(player_plane)
+function fire(sprite: Sprite, speed: number, kind: number) {
+    let proj = sprites.create(assets.image`projectile`, kind)
+    proj.setFlag(SpriteFlag.DestroyOnWall, true)
+    proj.setPosition(sprite.x, sprite.y)
+    calculate_velocity(sprite, proj, speed)
+}
+
+controller.A.onEvent(ControllerButtonEvent.Pressed, function player_fire() {
+    fire(player_plane, projectile_speed, SpriteKind.player_projectile)
+})
+function place_sprite(sprite: Sprite) {
+    let col = randint(1, grid.numColumns())
+    let row = randint(1, grid.numRows())
+    tiles.placeOnTile(sprite, tiles.getTileLocation(col, row))
+    if (spriteutils.distanceBetween(sprite, player_plane) < 120) {
+        place_sprite(sprite)
+    }
+    
+}
+
+function get_dir_to_player(enemy: Sprite): number {
+    let target_dir = spriteutils.angleFrom(enemy, player_plane)
+    target_dir = spriteutils.radiansToDegrees(target_dir) + 90
+    return target_dir
+}
+
+game.onUpdateInterval(4000, function spawn_enemy() {
+    let enemy = sprites.create(assets.image`enemy`, SpriteKind.Enemy)
+    sprites.setDataNumber(enemy, "turn", 0)
+    transformSprites.rotateSprite(enemy, 90)
+    place_sprite(enemy)
+    transformSprites.rotateSprite(enemy, get_dir_to_player(enemy))
+})
+sprites.onOverlap(SpriteKind.player_projectile, SpriteKind.Enemy, function destroy_enemy(proj: Sprite, enemy: Sprite) {
+    info.changeScoreBy(100)
+    enemy.destroy()
+})
+sprites.onOverlap(SpriteKind.enemy_projectile, SpriteKind.Player, function take_damage(proj: Sprite, player: Sprite) {
+    proj.destroy()
+    info.changeLifeBy(-1)
+})
+function hit_edge(sprite: Sprite, location: tiles.Location) {
+    transformSprites.changeRotation(sprite, 180)
+}
+
+scene.onHitWall(SpriteKind.Player, hit_edge)
+scene.onHitWall(SpriteKind.Enemy, hit_edge)
+function calculate_velocity(direction_sprite: Sprite, sprite: Sprite, speed: number) {
+    let direction = transformSprites.getRotation(direction_sprite)
+    direction = spriteutils.degreesToRadians(direction)
+    sprite.vx = Math.sin(direction) * speed
+    sprite.vy = Math.cos(direction) * -speed
+}
+
+function player_controls() {
+    if (controller.up.isPressed()) {
+        transformSprites.changeRotation(player_plane, -player_turn)
+    } else if (controller.down.isPressed()) {
+        transformSprites.changeRotation(player_plane, player_turn)
+    }
+    
+    calculate_velocity(player_plane, player_plane, player_speed)
+}
+
+function far_from_player(enemy: Sprite) {
+    let direction = transformSprites.getRotation(enemy)
+    if (get_dir_to_player(enemy) - direction < 3) {
+        sprites.setDataNumber(enemy, "turn", -enemy_turn)
+    }
+    
+    if (get_dir_to_player(enemy) - direction > 3) {
+        sprites.setDataNumber(enemy, "turn", enemy_turn)
+    }
+    
+}
+
+function enemy_behaviour(enemy: Sprite) {
+    if (spriteutils.distanceBetween(enemy, player_plane) > 80) {
+        far_from_player(enemy)
+    } else if (spriteutils.distanceBetween(enemy, player_plane) < 40) {
+        sprites.setDataNumber(enemy, "turn", enemy_turn)
+    } else {
+        sprites.setDataNumber(enemy, "turn", 0)
+    }
+    
+    transformSprites.changeRotation(enemy, sprites.readDataNumber(enemy, "turn"))
+    calculate_velocity(enemy, enemy, enemy_speed)
+    if (randint(1, 150) == 1) {
+        fire(enemy, projectile_speed - 50, SpriteKind.enemy_projectile)
+    }
+    
+}
+
+game.onUpdate(function tick() {
+    player_controls()
+    for (let enemy of sprites.allOfKind(SpriteKind.Enemy)) {
+        enemy_behaviour(enemy)
+    }
+})
